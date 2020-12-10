b5e81c5959d14e1f0a4d477f2190f4cbbed5ed46
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-12-10 03:02:35 +0200

    footer, about page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import Links from './components/links/Links';
 import Dashboard from './components/dashboard/Dashboard';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
+import LinkId from './components/links/components/LinkId';
+import About from './components/about/About';
+import Footer from './components/footer/Footer';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -15,7 +18,10 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route exact path="/links" component={Links} />
+          <Route exact path="/links/:id" component={LinkId} />
+          <Route exact path="/about" component={About} />
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
---
+++ client/src/components/about/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './About.css';
+
+//icons
+import WhatsAppLinkImage from './images/WhatsAppLinkImage.jpg';
+
+function About() {
+    return (
+        <div className="about">
+            {/* <img className="aboutImg" src={youtubeIcon} alt="About"/> */}
+            <p>You absolutely ask yourself what the hell is this site,</p>
+            <p>OK, </p>
+            <p>You'll Just about to get your answer.</p>
+            <img className="WhatsAppLinkImage" src={WhatsAppLinkImage} alt="WhatsApp"/>
+            <p>This Site is taking your chat history of your whatsapp and collect the links you and your friends shared each other</p>
+            <p>The home page includes charts with information, and the links page includes all the links of the group</p>
+            <p>Enjoy the Experience of the site!</p>
+        </div>
+    )
+}
+
+export default About
